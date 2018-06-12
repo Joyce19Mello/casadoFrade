61ad46f04fc1939c772d8ba6718d45cb0804f841
Joyce Melo <joycee.mello@hotmail.com>
2018-06-12 20:04:17 -0300

    primeiro passo

---
+++ assets/php/sendemail.php
@@ -0,0 +1,21 @@
+<?php
+
+	$name = @trim(stripslashes($_POST['name'])); 
+	$email = @trim(stripslashes($_POST['email']));  
+	$subject = @trim(stripslashes($_POST['subject']));  
+	$message = @trim(stripslashes($_POST['message'])); 
+
+	$email_from = $email;
+	$email_to = 'joycee.mello@hotmail.com'; 
+
+	$body = 'Name: ' . $name . "\n\n" . 'Email: ' . $email . "\n\n" . 'Subject: ' . $subject . "\n\n" . 'Message: ' . $message;
+
+	$success = @mail($email_to, $body, 'Name: ' . $name . "\n\n" . 'Email: ' . $email . "\n\n" . 'Subject: ' . $subject . "\n\n" . 'Message: ' . $message);
+	
+?>
+
+<!DOCTYPE HTML>
+<html lang="pt-br">
+<head>
+	<script>alert("Obrigado pelo contato, aguarde o retorno.");</script>
+</head>
